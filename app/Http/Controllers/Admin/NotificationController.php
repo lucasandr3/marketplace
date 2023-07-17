00a42b21a1ba9c82f7b03762886b08540e5aca70
Lucas <lucasvieiraandrade58@gmail.com>
2023-07-16 23:06:22 -0300

    FEAT: iniciao quotation e notificações

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -15,4 +15,25 @@ class NotificationController extends Controller
             'notifications' => $notifications
         ]);
     }
+
+    public function readOne($codeNotification)
+    {
+        $notification = auth()->user()->notifications()->find($codeNotification);
+        $notification->markAsRead();
+
+        flash('Notificação marcada como lida.');
+        return redirect()->back();
+    }
+
+    public function readAll()
+    {
+        $unreadNotifications = auth()->user()->unreadNotifications;
+
+        $unreadNotifications->each(function ($notification) {
+            $notification->markAsRead();
+        });
+
+        flash('Notificações marcadas como lidas.');
+        return redirect()->back();
+    }
 }
